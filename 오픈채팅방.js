bbaf7079da9bcfb604dabf8d3f376f8ff08fbb7d
Sangbin Lee <leesangbin@outlook.com>
2022-02-27 23:00:15 +0100

    add 오픈채팅방

---
+++ 오픈채팅방.js
@@ -0,0 +1,27 @@
+function parseItems(record) {
+    const mappings = {};
+    const actions = [];
+
+    record.forEach((item) => {
+        const [action, id, name] = item.split(' ');
+        if (name) {
+            mappings[id] = name;
+        }
+        if (action !== 'Change') {
+            actions.push({
+                message: action === 'Enter' ? '님이 들어왔습니다.' : '님이 나갔습니다.',
+                id,
+            });
+        }
+    });
+
+    return { mappings, actions };
+}
+
+function solution(record) {
+    const { mappings, actions } = parseItems(record);
+
+    const answer = actions.map((action) => `${mappings[action.id]}${action.message}`);
+
+    return answer;
+}
