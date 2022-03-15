61cc94992eee38c6c9b3445d6b231f059dc16c96
Sangbin Lee <leesangbin@outlook.com>
2022-03-15 15:45:57 +0100

    add n진수 게임 documentation

--- level2/[3차] n진수 게임.js
+++ level2/[3차] n진수 게임.js
@@ -1,5 +1,11 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17687
 
+/**
+ * 진수 변환
+ * @param {number} n 변환 할 10진수 숫자
+ * @param {string} digits 사용 할 숫자 문자열
+ * @returns {string}
+ */
 function convert(n, digits) {
     if (n === 0) {
         return digits[0];
@@ -17,6 +23,14 @@ function convert(n, digits) {
     return number;
 }
 
+/**
+ * 내 턴에 불러야 할 숫자들을 계산
+ * @param {string} digits 진수 문자열
+ * @param {number} length 미리 계산 할 길이
+ * @param {number} nbPlayers 게임에 참여하는 사람 수
+ * @param {number} turn 내 턴
+ * @returns {string[]} 내 턴마다 불러야 할 숫자
+ */
 function computeGame(digits, length, nbPlayers, turn) {
     let numbers = [];
     let toConvert = 0;
