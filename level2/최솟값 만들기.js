dcfe78644ae12e858216666956357b16a4a228e1
Sangbin Lee <leesangbin@outlook.com>
2022-04-15 23:12:04 +0200

    add 최솟값 만들기

---
+++ level2/최솟값 만들기.js
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12941
+
+function solution(A, B) {
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+
+    var answer = 0;
+
+    for (let i = 0; i < A.length; i++) {
+        answer += A[i] * B[i];
+    }
+
+    return answer;
+}
